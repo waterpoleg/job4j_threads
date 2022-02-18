29f951c7a3ceac74c708e4c83f5fd3ce623e121d
Oleg Korneev <waterpoleg@gmail.com>
2022-02-18 13:18:34 +0300

    1.1. Состояние нити. [#229175 #265974] - fix

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -9,7 +9,7 @@ public class ThreadState {
         System.out.println(second.getName() + " - " + second.getState());
         second.start();
         while (first.getState() != Thread.State.TERMINATED
-                && second.getState() != Thread.State.TERMINATED) {
+                || second.getState() != Thread.State.TERMINATED) {
             System.out.println("working...");
         }
         System.out.println("finished");
